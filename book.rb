a8e9c36c45298c8f45ba4910aa9858cf894fec15
rodk <rod2010rod2010@hotmail.com>
2016-06-10 22:36:12 -0400

    modify ex08book.rb

--- book.rb
+++ book.rb
@@ -1,40 +1,39 @@
 class Book
-
   # initialize is not necessary for this code!
-  def initialize (title="")
+  def initialize (title)
     @book = title
   end
 
-  def title(title)
+  def title
   	cap_title = []
 #  	wstr = Array.new  # do not have to do this since 'split(" ")' will create the array!
-  	wstr = title.split(" ")
-
+  	wstr = @book.split(" ") # THIS IS THE ANSWER!!!
     wstr.each do |x|
     x.capitalize! if !%w(the a an and in of for from).include?(x) || %w(i).include?(x)
     cap_title << x
     end
     cap_title[0].capitalize!
-
-    @book = cap_title.join(" ")   # THIS IS THE ANSWER!!!
-
+    return cap_title.join(" ")   
   end
 
 end
 
 #create object 'book'
-book = Book.new
-
-p book.title("inferno")
-p book.title("stuart little")
-p book.title("alexander the great")
-p book.title("to kill a mockingbird")
-p book.title("to eat an apple a day")
-p book.title("war and peace")
-p book.title("love in time of cholera")
-p book.title("what i wish i knew when i was 20")
-p book.title("the man in the iron mask")
-
-
-
-  
+book = Book.new("inferno")
+p book.title
+book = Book.new("stuart little")
+p book.title
+book = Book.new("alexander the great")
+p book.title
+book = Book.new("to kill a mockingbird")
+p book.title
+book = Book.new("to eat an apple a day")
+p book.title
+book = Book.new("war and peace")
+p book.title
+book = Book.new("love in time of cholera")
+p book.title
+book = Book.new("what i wish i knew when i was 20")
+p book.title
+book = Book.new("the man in the iron mask")
+p book.title
