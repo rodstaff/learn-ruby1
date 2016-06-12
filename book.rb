b961d3561a21180bbf3958107b1cc194c0ff3b8c
rodk <rod2010rod2010@hotmail.com>
2016-06-12 17:41:34 -0400

    modify ex08book.rb

--- book.rb
+++ book.rb
@@ -1,39 +1,59 @@
 class Book
-  # initialize is not necessary for this code!
-  def initialize (title)
-    @book = title
+  # initialize is necessary so that the instance @title can be accessed within the class
+  def initialize (title="")
+    @title = title
   end
+
+  attr_accessor :title
 
   def title
   	cap_title = []
 #  	wstr = Array.new  # do not have to do this since 'split(" ")' will create the array!
-  	wstr = @book.split(" ") # THIS IS THE ANSWER!!!
+  	wstr = @title.split(" ")      
     wstr.each do |x|
     x.capitalize! if !%w(the a an and in of for from).include?(x) || %w(i).include?(x)
     cap_title << x
     end
     cap_title[0].capitalize!
-    return cap_title.join(" ")   
+    return cap_title.join(" ")   # the 'return' is optional
   end
 
 end
 
 #create object 'book'
-book = Book.new("inferno")
-p book.title
-book = Book.new("stuart little")
-p book.title
-book = Book.new("alexander the great")
-p book.title
-book = Book.new("to kill a mockingbird")
-p book.title
-book = Book.new("to eat an apple a day")
-p book.title
-book = Book.new("war and peace")
-p book.title
-book = Book.new("love in time of cholera")
-p book.title
-book = Book.new("what i wish i knew when i was 20")
-p book.title
-book = Book.new("the man in the iron mask")
-p book.title
+@book = Book.new
+@book.title = "inferno"
+p @book.title
+
+@book = Book.new
+@book.title = "stuart little"
+p @book.title
+
+@book = Book.new
+@book.title = "alexander the great"
+p @book.title
+
+@book = Book.new
+@book.title = "to kill a mockingbird"
+p @book.title
+
+@book = Book.new
+@book.title = "to eat an apple a day"
+p @book.title
+
+@book = Book.new
+@book.title = "war and peace"
+p @book.title
+
+@book = Book.new
+@book.title = "love in time of cholera"
+p @book.title
+
+@book = Book.new
+@book.title = "what i wish i knew when i was 20"
+p @book.title
+
+@book = Book.new
+@book.title = "the man in the iron mask"
+p @book.title
+
